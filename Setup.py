5a615932a7b0238543dfa8f70a89c74b44f216b9
Tristan B. Kildaire <tristankildaire@gmail.com>
2014-10-01 17:30:08 +0200

    Adedd whitespaces. Added new lines with `\n` rather than `print()`.

--- Setup.py
+++ Setup.py
@@ -1,23 +1,27 @@
 'DeavmiOSS Mirror Python Setup Script - GNU GPL v3.0 or above'
-import subprocess
+
+import os
+
 class Configuration():
+
     default_text_editor = "nano"
+
 config = Configuration()
+
 print("DeavmiOSS Mirror Python Setup Script - GNU GPL v3.0 or above")
 def bye():
-    print()
-    print("Your mirror has been setup!")
-    print()
-    print("Thank you for using this tool!")
-    print()
+
+    print("\nYour mirror has been setup!\n")
+    print("Thank you for using this tool!\n")
     exit()
+
 def update_status():
-    print()
-    prompt = input("Change the 'Status' file to display True records? (Y/n): ")
+
+    prompt = input("\nChange the 'Status' file to display True records? (Y/n): ")
     if prompt == "yes" or "Yes":
         'Credits to this page here, for helping with the cd command - <http://techie-buzz.com/foss/how-to-go-back-to-previous-directory-and-home-directory-in-linux-shell-or-putty.html>'
-        subprocess.Popen("cd")
-        subprocess.Popen(config.default_text_editor + " Status")
+        os.system("cd")
+        os.system(config.default_text_editor + " Status")
         bye()
     elif prompt == "no" or "No":
         print("'Status' will not be changed. [Skipped]")
@@ -25,14 +29,15 @@ def update_status():
     else:
         print("'" + prompt + "' is an invalid option, please retry!")
         update_status()
+
 def update_html():
-    print()
+
     print("We recommend you change the 'index.html' file to your needs.")
     prompt = input("Change 'index.html' file now?: ")
     if prompt == "yes" or "Yes":
         'Credits to this page here, for helping with the cd command - <http://techie-buzz.com/foss/how-to-go-back-to-previous-directory-and-home-directory-in-linux-shell-or-putty.html>'
-        subprocess.Popen("cd")
-        subprocess.Popen(config.default_text_editor + " index.html")
+        os.system("cd")
+        os.system(config.default_text_editor + " index.html")
         update_status()
     elif prompt == "no" or "No":
         print("'index.html' will not be changed. [Skipped]")
@@ -40,21 +45,23 @@ def update_html():
     else:
         print("'" + prompt + "' is an invalid option, please retry!")
         update_html()
+
 def get_images():
+
     print("Reading configuration...")
     if iso_64bit == True:
         print("64-Bit ISO is 'True'!")
         print("=========================")
         print()
         print("Creating directory...")
-        subprocess.Popen("mkdir 64-Bit")
+        os.system("mkdir 64-Bit")
         print("Creating directory... [Done]")
         print("Fetching 64-Bit Image File...")
-        subprocess.Popen("cd 64-Bit")
-        subprocess.Popen("wget https://my-mirror.com/Debian/Debian-64-Bit.iso")
+        os.system("cd 64-Bit")
+        os.system("wget https://my-mirror.com/Debian/Debian-64-Bit.iso")
         print("Fetching 64-Bit Image File... [Done]")
         print("Cleaning up...")
-        subprocess.Popen("cd")
+        os.system("cd")
         print("Cleaning up... [Done]")
     else:
         print("64-Bit ISO is 'False'!")
@@ -68,14 +75,14 @@ def get_images():
         print("=========================")
         print()
         print("Creating directory...")
-        subprocess.Popen("mkdir 32-Bit")
+        os.system("mkdir 32-Bit")
         print("Creating directory... [Done]")
         print("Fetching 32-Bit Image File...")
-        subprocess.Popen("cd 32-Bit")
-        subprocess.Popen("wget https://my-mirror.com/Debian/Debian-32-Bit.iso")
+        os.system("cd 32-Bit")
+        os.system("wget https://my-mirror.com/Debian/Debian-32-Bit.iso")
         print("Fetching 32-Bit Image File... [Done]")
         print("Cleaning up...")
-        subprocess.Popen("cd")
+        os.system("cd")
         print("Cleaning up... [Done]")
     else:
         print("32-Bit ISO is 'False'!")
@@ -85,14 +92,13 @@ def get_images():
         print("Directory creation is null!")
         print("ISO download is null!")
 finished()
+
 def iso_directory():
-    print("List of all isos available:")
-    print()
+
+    print("List of all isos available:\n")
     print("1. 64-Bit")
-    print("2. 32-Bit")
-    print()
-    print("Type 'done' when you are done adding the iso image files.")
-    print()
+    print("2. 32-Bit\n")
+    print("Type 'done' when you are done adding the iso image files.\n")
     choose_iso = input("Which of the following would you like to have in your mirror?: ")
     if choose_iso == "1":
         iso_64Bit = True
@@ -105,17 +111,19 @@ def iso_directory():
     else:
         print("'" + prompt + "' is an invalid option, please retry!")
         iso_directory()
+
 def fetch_isos():
+
   prompt = input("Would you like to fetch the .isos now?")
   if prompt = "yes" or "Yes":
       iso_directory()
       print("Fetching files now (.isos/disk-images)...")
       print("Fetching 64-Bit .iso/disk-image...")
-      subprocess.Popen("cd 64-Bit")
-      subprocess.Popen("wget http://mirror.com/debian-64Bit.iso")
+      os.system("cd 64-Bit")
+      os.system("wget http://mirror.com/debian-64Bit.iso")
       print("Fetching 32-Bit .iso/disk-image...")
-      subprocess.Popen("cd 32-Bit")
-      subprocess.Popen("wget http://mirror.com/debian-32Bit.iso")
+      os.system("cd 32-Bit")
+      os.system("wget http://mirror.com/debian-32Bit.iso")
       print("Fetching files now (.isos/disk-images)... [Done]")
   if prompt == "no" or "No"
       print(".isos/disk-images won't be fetched!")
@@ -127,20 +135,21 @@ def getfiles_html():
   prompt = input("Use Wget or Git for fetching files?")
   if prompt == "wget":
       print("fetching files using Wget...")
-      subprocess.Popen("wget https://raw.githubusercontent.com/DeavmiOSS/mirror_service/master/index.html")
-      subprocess.Popen("wget https://raw.githubusercontent.com/DeavmiOSS/mirror_service/master/Status")
-      subprocess.Popen("wget https://raw.githubusercontent.com/DeavmiOSS/mirror_service/master/logo.png")
+      os.system("wget https://raw.githubusercontent.com/DeavmiOSS/mirror_service/master/index.html")
+      os.system("wget https://raw.githubusercontent.com/DeavmiOSS/mirror_service/master/Status")
+      os.system("wget https://raw.githubusercontent.com/DeavmiOSS/mirror_service/master/logo.png")
       print("fetching files using Wget... [Done]")
   elif prompt == "git":
       print("Cloning Git repository...")
-      subprocess.Popen("git clone https://github.com/DeavmiOSS/mirror_service.git \")
+      os.system("git clone https://github.com/DeavmiOSS/mirror_service.git \")
       print("Cloning Git repository... [Done]")
   else:
       print("'" + prompt + "' is an invalid option, please retry!")
       getfiles_html()
-  fetch_isos()
-print()
+      fetch_isos()
+
 def app_start():
+
     prompt = input("Setup miror now? (Y/n): ")
     if prompt == "Y" or "y":
         getfiles_html()
@@ -149,5 +158,6 @@ def app_start():
     else:
         print("'" + prompt + "' is an invalid option, please retry!")
         app_start()
+
 'Start the application'
 app_start()
